4dd5dd8d9792307f9947e3d308f5052002849c49
John Anderson <panic@semiosix.com>
2014-03-06 16:47:47 +0200

    specs. finally. for Mouth and Method

---
+++ spec/core_extensions_spec.rb
@@ -0,0 +1,50 @@
+require 'rspec'
+
+require Pathname(__dir__) + '../lib/wyrm/core_extensions.rb'
+
+describe Method do
+  describe '#kwargs_as_hash' do
+    it 'empty for no keywords' do
+      inst = Class.new do
+        def without_kwargs( one, two )
+          @kwargs = method(__method__).kwargs_as_hash(binding)
+        end
+      end.new
+      inst.without_kwargs :one, :two
+      inst.instance_variable_get('@kwargs').should == {}
+    end
+
+    it 'gives back hash of keywords' do
+      inst = Class.new do
+        def with_kwargs( one: 'one', two: 'two')
+          @kwargs = method(__method__).kwargs_as_hash(binding)
+        end
+      end.new
+
+      inst.with_kwargs
+      inst.instance_variable_get('@kwargs').should == {one: 'one', two: 'two'}
+    end
+
+    it 'has correct values' do
+      inst = Class.new do
+        def with_kwargs( one: 'one', two: 'two')
+          @kwargs = method(__method__).kwargs_as_hash(binding)
+        end
+      end.new
+
+      inst.with_kwargs( one: 1, two: 2 )
+      inst.instance_variable_get('@kwargs').should == {one: 1, two: 2}
+    end
+
+    it 'gets some default values' do
+      inst = Class.new do
+        def with_kwargs( one: 'one', two: 'two')
+          @kwargs = method(__method__).kwargs_as_hash(binding)
+        end
+      end.new
+
+      inst.with_kwargs( one: 1 )
+      inst.instance_variable_get('@kwargs').should == {one: 1, two: 'two'}
+    end
+  end
+end
